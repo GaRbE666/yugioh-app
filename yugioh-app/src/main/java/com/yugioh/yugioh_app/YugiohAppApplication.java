124f1336ddbcb7c2d03f43d61dfb4eb27cb1990a
GaRbE666 <jaimegon17@gmail.com>
2025-05-17 14:59:12 +0200

    First commit
    Primero commit con las configuraciones iniciales

---
+++ yugioh-app/src/main/java/com/yugioh/yugioh_app/YugiohAppApplication.java
@@ -0,0 +1,13 @@
+package com.yugioh.yugioh_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YugiohAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YugiohAppApplication.class, args);
+	}
+
+}
